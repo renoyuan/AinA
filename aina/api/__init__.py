bfa0daf5d6672876d51700d642972acc21c7d38d
renoyuan <renoyuan@foxmail.com>
2024-07-22 17:32:51 +0800

    create project

---
+++ aina/api/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# PROJECT_NAME: AinA __init__.py
+# CREATE_TIME: 2024/7/22 16:29
+# E_MAIL: renoyuan@foxmail.com
+# AUTHOR: renoyuan
+# note:
---
+++ aina/server.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# PROJECT_NAME: AinA server
+# CREATE_TIME: 2024/7/22 16:28
+# E_MAIL: renoyuan@foxmail.com
+# AUTHOR: renoyuan
+# note: server
+
+
